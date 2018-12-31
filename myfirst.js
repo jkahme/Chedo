19cc918e0971587ecca15e5414e91f3a4b29ac7a
jkahme <46284077+jkahme@users.noreply.github.com>
2018-12-31 14:31:11 -0600

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -5,6 +5,3 @@ res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
 res.end('<h1>Hello World</h1>');
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
