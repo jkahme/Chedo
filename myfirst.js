c20f49725f1c4aeb17b1fb40a10df500fe054f46
jkahme <46284077+jkahme@users.noreply.github.com>
2018-12-31 14:35:52 -0600

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -4,7 +4,9 @@ const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
 res.end('<h1>Hello World</h1>');
+moment().calendar(); 
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
 });
+
