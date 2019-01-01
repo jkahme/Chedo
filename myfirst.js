60dbf10a93b2a5bb5662b348ec175a05bf52f1c1
jkahme <46284077+jkahme@users.noreply.github.com>
2018-12-31 21:39:01 -0600

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,6 @@
 var http = require('http');
-var server = http.createServer(function(request, response) {
+http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");                    
-});
-var port = process.env.PORT || 1337;
-server.listen(port);
-console.log("Server running at http://localhost:%d", port);
+}).listen(1337);
+console.log("Server running at http://localhost:%d", 1337);
