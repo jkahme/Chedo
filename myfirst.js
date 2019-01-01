080cdb2567d54a2783402d05ebb79b720e778e59
jkahme <46284077+jkahme@users.noreply.github.com>
2018-12-31 21:25:06 -0600

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var time = new Date(Date.now()).toLocaleString();
 var server = http.createServer(function(request, response) {
-
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
+    response.writeBody(200,{"Content-Type"});
+    response.end(time);                        
 });
 var port = process.env.PORT || 1337;
 server.listen(port);
